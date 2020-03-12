7dc90056f9d56c563db840c602de2a4894a0df57
Walter Kuijlaars <Walter.Kuijlaars@vanderlande.com>
2020-03-12 21:20:25 +0100

    created docs folder

---
+++ docs/precache-manifest.373519e8f28eae069f95d18f01ec7909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd7b5da7427a512aa1b03a6b74816ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6972f24a8443daa764",
+    "url": "/static/css/main.1fbe5368.chunk.css"
+  },
+  {
+    "revision": "ec3b7f9c9c5febf0c451",
+    "url": "/static/js/2.182e20e5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.182e20e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6972f24a8443daa764",
+    "url": "/static/js/main.6b0844a9.chunk.js"
+  },
+  {
+    "revision": "6099eb3da3cd9804c5e4",
+    "url": "/static/js/runtime-main.6f0f6973.js"
+  }
+]);
